3fa55e3a484da10d8bdf8225b4fcf2d79d5d8b24
klickmanufaktur <patrick@klickmanufaktur.de>
2024-11-19 13:34:29 +0100

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,52 @@
 const gulp = require("gulp");
-const sass = require("gulp-dart-sass"); // Verwende gulp-dart-sass
+const sass = require("gulp-dart-sass");
 const cleanCSS = require("gulp-clean-css");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sourcemaps = require("gulp-sourcemaps");
-const gulpIf = require("gulp-if");
-const yargs = require("yargs");
-
-// Flags
-const argv = yargs.argv;
-const isProduction = argv.prod; // --prod Flag für Produktionsmodus
-const shouldMinify = argv.min;  // --min Flag für CSS-Minimierung
+const rename = require("gulp-rename"); // Zum Umbenennen von Dateien
 
 // Pfade
 const paths = {
     styles: {
         main: "app/styles.sass", // Nur styles.sass wird kompiliert
-        watch: "app/**/*.sass",  // Alle SASS-Dateien für Watcher
+        watch: "app/**/*.sass",  // Alle SASS-Dateien im app-Ordner und Unterordnern
         dest: "dist/",
     },
 };
 
-// SASS kompilieren
-function styles() {
+// Generiert die unminifizierte styles.css
+function stylesPlain() {
     return gulp
         .src(paths.styles.main)
-        .pipe(gulpIf(isProduction, sourcemaps.init())) // Sourcemaps starten, nur bei --prod
-        .pipe(sass({
-            silenceDeprecations: ['legacy-js-api'],
-        }))
-        .pipe(gulpIf(isProduction || shouldMinify, postcss([autoprefixer()]))) // Autoprefixer für prod oder --min
-        .pipe(gulpIf(isProduction || shouldMinify, cleanCSS())) // Minimierung bei --prod oder --min
-        .pipe(gulpIf(isProduction, sourcemaps.write('.'))) // Sourcemaps schreiben
-        .pipe(gulp.dest(paths.styles.dest));
+        .pipe(sass().on("error", sass.logError)) // Nur SASS kompilieren
+        .pipe(rename("styles.css")) // Umbenennen in styles.css
+        .pipe(gulp.dest(paths.styles.dest)); // Ziel: dist/styles.css
+}
+
+// Generiert die minifizierte styles-min.css mit Autoprefixer und Sourcemap
+function stylesMinified() {
+    return gulp
+        .src(paths.styles.main)
+        .pipe(sourcemaps.init()) // Startet Sourcemaps
+        .pipe(sass().on("error", sass.logError)) // Kompiliert SASS
+        .pipe(postcss([autoprefixer()])) // Fügt Autoprefixer hinzu
+        .pipe(cleanCSS()) // Minifiziert CSS
+        .pipe(rename("styles-min.css")) // Umbenennen in styles-min.css
+        .pipe(sourcemaps.write(".")) // Schreibt Sourcemap (styles-min.css.map)
+        .pipe(gulp.dest(paths.styles.dest)); // Ziel: dist/styles-min.css
 }
 
 // Watch Task für Änderungen
 function watch() {
-    gulp.watch(paths.styles.watch, styles); // Watch alle SASS-Dateien
+    gulp.watch(paths.styles.watch, gulp.series(stylesPlain, stylesMinified));
 }
 
-// Tasks exportieren
-exports.styles = styles;
-exports.watch = watch;
-
-// Standard-Task für Entwicklung
-exports.default = gulp.series(styles, watch);
+// Standard-Task (gulp)
+exports.default = gulp.series(stylesPlain, stylesMinified);
 
 // Build-Task für Prod
-exports.build = gulp.series(styles);
+exports.build = gulp.series(stylesPlain, stylesMinified);
+
+// Watch Task
+exports.watch = watch;
